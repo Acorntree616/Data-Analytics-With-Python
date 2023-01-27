62cfadcfddf807eb8fce117331fad733390c48fb
Alex Cornwall <acornwall@wsu.edu>
2023-01-26 18:38:11 -0800

    after 12623 class

--- Module1-Intro_to_Python/day5_bad.py
+++ Module1-Intro_to_Python/day5_bad.py
@@ -1,18 +1,18 @@
 from sys import argv
 from os.path import exists
 
-script, num1, num2, num2, num3, out_filename = argv
+script, num1, num2, num3, num4, num5, out_filename = argv
 
-file_exists = exsits(out_filename)
+file_exists = exists(out_filename)# True or False wether the file exists or not.
 print("Does the output file exist? {}".format(file_exists))
 print("Writing output to {}".format(out_filename))
 
-out_file = open(out_filename)
+out_file = open(out_filename, "w")
 outdata = out_file
 print("Waiting... Type the RETURN key to continue")
-  input()
+input()
 
-answer = (num1 + num2 + num2 + num3 + num4) / (len(argv) - 1)
+answer = (float(num1) + float(num2) + float(num3) + float(num4) + float(num5)) / (len(argv) - 2)
 
 out_file.write("{:.2f}\n".format(answer))
 print("\a")

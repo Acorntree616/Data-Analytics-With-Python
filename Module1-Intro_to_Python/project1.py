604b7be93ed4bcb514aa74b07e4f3b152dd139d1
Alex Cornwall <acornwall@wsu.edu>
2023-02-10 06:43:39 -0800

    project done, almost

--- Module1-Intro_to_Python/project1.py
+++ Module1-Intro_to_Python/project1.py
@@ -4,33 +4,78 @@ This file perform.
 Author: Alex Cornwall
    
 """
-def main():
-    """
-    This function ensures that the code is run when executed on a command line.
-   
-    """
-    # Global variables that are associated with data collection regions
-    AREAS = {
-        0: 'All',
-        1: 'Rawah Wilderness Area',
-        2: 'Neota Wilderness Area',
-        3: 'Comanche Peak Wilderness Area',
-        4: 'Cache la Poudre Wilderness Area'
-    }
+from sys import argv
+ # Global variables that are associated with data collection regions
+AREAS = {
+    0: 'All',
+    1: 'Rawah Wilderness Area',
+    2: 'Neota Wilderness Area',
+    3: 'Comanche Peak Wilderness Area',
+    4: 'Cache la Poudre Wilderness Area'
+}
 
-    #Global variables that are associated with data collection types
-    COLUMNS = {
-        0: 'Elevation',
-        1: 'Aspect',
-        2: 'Slope',
-        3: 'Horizontal_Distance_To_Hydrology',
-        4: 'Vertical_Distance_To_Hydrology',
-        5: 'Horizontal_Distance_To_Roadways',
-        6: 'Hillshade_9am',
-        7: 'Hillshade_Noon',
-        8: 'Hillshade_3pm',
-        9: 'Horizontal_Distance_To_Fire_Points'
-    }
+#Global variables that are associated with data collection types
+COLUMNS = {
+    0: 'Elevation',
+    1: 'Aspect',
+    2: 'Slope',
+    3: 'Horizontal_Distance_To_Hydrology',
+    4: 'Vertical_Distance_To_Hydrology',
+    5: 'Horizontal_Distance_To_Roadways',
+    6: 'Hillshade_9am',
+    7: 'Hillshade_Noon',
+    8: 'Hillshade_3pm',
+    9: 'Horizontal_Distance_To_Fire_Points'
+}
+
+def pull_data(area_dict, cols):
+    """[Summary]
+
+    :param [ParamName]: [ParamDescription], defaults to [DefaultParamVal]
+    :type [ParamName]: [ParamType](, optional)
+    ...
+    :raises [ErrorType]: [ErrorDescription]
+    ...
+    :return: [ReturnDescription]
+    :rtype: [ReturnType]
+    """
+    for c in area_dict.keys():
+        area_dict[c].append(float(cols[c]))
+            
+def call_report(stats_repos, area_dict, Name):
+    """[Summary]
+
+    :param [ParamName]: [ParamDescription], defaults to [DefaultParamVal]
+    :type [ParamName]: [ParamType](, optional)
+    ...
+    :raises [ErrorType]: [ErrorDescription]
+    ...
+    :return: [ReturnDescription]
+    :rtype: [ReturnType]
+    """
+    #writing in my wilderness area names
+    stats_repos.write('--------------------------------------\n')
+    stats_repos.write('{}\n'.format(Name))
+    stats_repos.write('--------------------------------------\n')
+    for c in area_dict.keys():
+        stats_repos.write("Minimum {}: {}\n".format(COLUMNS[c], min(area_dict[c])))
+        stats_repos.write("Maximum {}: {}\n".format(COLUMNS[c], max(area_dict[c])))
+        stats_repos.write("Mean {}: {}\n".format(COLUMNS[c], round((sum(area_dict[c])/len(area_dict[c])))))
+        stats_repos.write("\n")    
+    
+def main():
+    
+    """[Summary]This function ensures that the code is run when executed on a command line.
+
+    :param [ParamName]: [ParamDescription], defaults to [DefaultParamVal]
+    :type [ParamName]: [ParamType](, optional)
+    ...
+    :raises [ErrorType]: [ErrorDescription]
+    ...
+    :return: [ReturnDescription]
+    :rtype: [ReturnType]
+  
+    """
     
     Data = {
         0: [],
@@ -42,7 +87,7 @@ def main():
         6: [],
         7: [],
         8: [],
-        9: []
+        9: [],
     }
     
     Rawah = {
@@ -55,7 +100,7 @@ def main():
         6: [],
         7: [],
         8: [],
-        9: []
+        9: [],
     }
     
     Neota = {
@@ -68,7 +113,7 @@ def main():
         6: [],
         7: [],
         8: [],
-        9: []
+        9: [],
     }
     
     Comanche = {
@@ -81,7 +126,7 @@ def main():
         6: [],
         7: [],
         8: [],
-        9: []
+        9: [],
     }
     
     Cache = {
@@ -94,56 +139,40 @@ def main():
         6: [],
         7: [],
         8: [],
-        9: []
+        9: [],
     }
         
-    from sys import argv
+    
     script, file_name = argv
     file = open(file_name, encoding="UTF-8")
-    
-    #for row in file:
-       # cols = row.split(",")
-    def pull_data(area):    
-        for c in Data.keys():
-            x[c].append(float(cols[c]))
-            
-    def call_report(location):
-        for report in location.keys():
-            print("Minimum {}: {}".format(COLUMNS[report], min(x[report])))
-            print("Maximum {}: {}".format(COLUMNS[report], max(x[report])))
-            print("Mean {}: {}".format(COLUMNS[report], round((sum(x[report])/len(x[report])))))
-            print("\n")
+    stats_repos = open('./data/Data_summary.txt', 'w')    
      
     for row in file:
         cols = row.split(",")
-        rawah, neota, comanche, cache = int(cols[11]), int(cols[12]), int(cols[13]), int(cols[14])
+        rawah, neota, comanche, cache = int(cols[10]), int(cols[11]), int(cols[12]), int(cols[13])
+        pull_data(Data, cols)
         if rawah == 1:
-            x = Rawah
-            pull_data(cols)
+            pull_data(Rawah, cols)
             
         elif neota == 1:
-            x = Neota
-            pull_data(cols)
+            pull_data(Neota, cols)
             
         elif comanche == 1:
-            x = Comanche
-            pull_data(cols)
-            
-                
+            pull_data(Comanche, cols)
+             
         elif cache == 1:
-            x = Cache
-            pull_data(cols)
+            pull_data(Cache, cols)
  
-    
-    call_report(Rawah)
-    call_report(Neota)
-    call_report(Comanche)
-    call_report(Cache)
-    
-    
+    call_report(stats_repos, Data, AREAS[0])
+    call_report(stats_repos, Rawah, AREAS[1])
+    call_report(stats_repos, Neota, AREAS[2])
+    call_report(stats_repos, Comanche, AREAS[3])
+    call_report(stats_repos, Cache, AREAS[4])
     
     
     
+    
+    stats_repos.close()
     file.close()
 
 
